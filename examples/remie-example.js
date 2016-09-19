7b778c494275389814c6f5e17c06d3eaf051c0c5
Nicholas Reno <nick@livioradio.com>
2016-09-19 17:29:08 -0400

    removed commented expect values

--- examples/remie-example.js
+++ examples/remie-example.js
@@ -10,7 +10,7 @@ var Remie = require('../libs/index.js'),
 options.internalMessage = 'I\'m the internal message for developer eyes only'
 // explain
 options.level = 'error'
-// exlain
+// explain
 options.code = 'server.400.forbidden'
 
 var i18next = require('i18next')
@@ -36,69 +36,35 @@ i18next.init({
 options.i18next = i18next
 
 console.log("\n\n/* ******************** Rich Error Default Example ******************** */\n");
-var defRich = remie.create();
-console.log(defRich)
-// expected to log:
-// RichError {}
-console.log("\n\n/* ******************** Rich Error Example ******************** */\n");
-var exRich = remie.create(myErr, options).toResponseObject() // call toResponseObject to remove any empty properties
-console.log(exRich)
-/* expected to log:
-{ error: 
-  { message: 'Something went wrong',
-    stack: 'Error: Something went wrong\n    at /Users/You/wherever/remie/examples/remie-example.js\n at here\n at there' },
-  level: 'error',
-  statusCode: 500 }
 
-error.stack will vary*/
+var defRich = remie.create();
+
+console.log(defRich)
+
+
+console.log("\n\n/* ******************** Rich Error Example ******************** */\n");
+
+var exRich = remie.create(myErr, options)
+
+console.log(exRich)
+
+
 console.log("\n\n/* ******************** Rich Error With A Caught Error ******************** */\n");
+
 try {
   let exRich = notReal.create("Something went wrong", {}); // calls a function that doesn't exist, throws error
 } catch (e) { // error is caught here
   var exRemie = remie.create(e, options) // uses the error and options to create new RichError
 
   console.log(exRemie)
-/*
-RichError {
-  error: 
-   { ReferenceError: notReal is not defined
-       at Object.<anonyomous> (/Users/You/wherever/remie/examples/remie-example.js)
-       at here
-       at there
-       at file:line:col code: undefined },
-  internalOnly: false,
-  internalMessage: 'I\'m the internal message for developer eyes only',
-  level: 'error',
-  messageData: undefined,
-  options: 
-   { internalMessage: 'I\'m the internal message for developer eyes only',
-     level: 'error',
-     code: 'server.400.forbidden' },
-  referenceData: undefined,
-  statusCode: 500 }
-*/
+
+
   console.log("\n\n/* ******************** Rich Error toResponseObject ******************** */\n");
+
   console.log(exRemie.toResponseObject())
-/*
-{ error: 
-   { message: 'notReal is not defined',
-     stack: 'ReferenceError: notReal is not defined\n    at Object.<anonymous> (/Users/You/wherever/remie/examples/remie-example.js)\n    at here\n    at there\n    at file:line:col' },
-  level: 'error',
-  statusCode: 500 }
-*/
+
+
   console.log("\n\n/* ******************** Rich Error removeEmptyProps ******************** */\n");
+
   console.log(remie.create(e).removeEmptyProps())
-/*
-RichError {
-  error: 
-   ReferenceError: notReal is not defined
-      at Object.<anonymous> (/Users/You/wherever/remie/examples/remie-example.js)
-      at here
-      at there
-      at file:line:col,
-  internalOnly: false,
-  level: 'error',
-  options: {},
-  statusCode: 500 }
-*/
 }
